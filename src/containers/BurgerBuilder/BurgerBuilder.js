a980c61eabd4b72a12b23ed00f31e1547a9f59ce
kunal.c@zestmoney.in <kunal.ch1812@gmail.com>
2020-01-12 19:08:32 +0530

    added backdrop in background when modal is open

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -82,7 +82,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
           <OrderSummary
             ingredients={this.state.ingredients}
             price={this.state.totalPrice}
