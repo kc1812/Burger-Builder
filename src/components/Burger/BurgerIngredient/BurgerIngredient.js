7e4900b942dc59ecae3d464964c4ceed3898eb65
kunal.c@zestmoney.in <kunal.ch1812@gmail.com>
2020-01-12 18:33:03 +0530

    css changes and removed exta semicolon

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -12,8 +12,8 @@ class BurgerIngredient extends Component {
       case ('bread-top'):
         ingredients = (
           <div className={classes.BreadTop}>
-            <div className={classes.Seeds1}></div>;
-            <div className={classes.Seeds2}></div>;
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
           </div>
         );
         break;
