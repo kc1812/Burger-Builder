6d95b0c46cfc4d0ae982a45dd41f22b272642dba
kunal.c@zestmoney.in <kunal.ch1812@gmail.com>
2020-01-14 14:19:12 +0530

    Added Sidedrawer with logo and navigation items, adjusted the sidedrawer for mobile first and toolbar for desktop first

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/Auxilary';
 import Classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = props => {
   return (
     <Aux>
       <Toolbar />
+      <SideDrawer />
       <main className={Classes.Content}>
         {props.children}
       </main>
