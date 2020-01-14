ca89fb84ddad9145e9857784f0a8f2c6114b0d7e
Kunal <kunal.ch1812@gmail.com>
2020-01-14 16:04:51 +0530

    Moved layout to hoc folder and Auxilary to Auxiliary folder inside hoc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './components/Layouts/Layout';
+import Layout from './hoc/Auxilary/Auxilary';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
