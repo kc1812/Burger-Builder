d277c45b5578862b8872a9af5bc890c1ae32fe26
Kunal <kunal.ch1812@gmail.com>
2020-01-14 16:19:47 +0530

    path fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './hoc/Auxilary/Auxilary';
+import Layout from './hoc/Layouts/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
