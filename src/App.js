58adb69a79e92df1195958d45f3aef8a3cdaf30e
kunal.c@zestmoney.in <kunal.ch1812@gmail.com>
2020-05-18 23:56:30 +0530

    setup checkout and  routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from './hoc/Layouts/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     </div>
   );
